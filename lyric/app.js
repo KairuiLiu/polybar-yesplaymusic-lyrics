8086ac1678e0763f624614926c894718bc10bdff
LiuKairui <tclkr@live.com>
2022-03-26 17:34:51 +0800

    feat: use_cache_reduce_latency

--- lyric/app.js
+++ lyric/app.js
@@ -1,9 +1,6 @@
 const fetch = require('node-fetch');
-
-function form2bit(v) {
-  if (v < 10) return '0' + v;
-  return '' + v;
-}
+const path = require('path');
+const fs = require('fs');
 
 async function reqLyrics() {
   const st = new Date();
@@ -14,28 +11,53 @@ async function reqLyrics() {
       process.exit(1);
     }
   );
-  const lyricsRes = await fetch(
-    `http://127.0.0.1:10754/lyric?id=${playerRes.currentTrack.id}`
-  ).then((response) => response.json());
+  let lyrics = await fs.readFileSync(
+    path.join(__dirname, '.lyric.json'),
+    'utf8'
+  );
+  if (lyrics) {
+    lyrics = JSON.parse(lyrics);
+    if (lyrics.id !== playerRes.currentTrack.id) lyrics = null;
+  }
+  if (!lyrics) {
+    const lyricsRes = await fetch(
+      `http://127.0.0.1:10754/lyric?id=${playerRes.currentTrack.id}`
+    ).then((response) => response.json());
+    lyrics = {
+      id: playerRes.currentTrack.id,
+      lyrics: lyricsRes.lrc.lyric.split('\n').map((d) => {
+        const reg = /^\[([0-9]{2}):([0-9]{2})\.([0-9]*)\](.*)/.exec(d);
+        if (reg)
+          return {
+            time:
+              Number.parseInt(reg[1], 10) * 60 +
+              Number.parseInt(reg[2], 10) +
+              Number.parseInt(reg[3], 10) / 1000,
+            text: reg[4],
+          };
+        return { time: Number.MAX_SAFE_INTEGER, text: '' };
+      }),
+    };
+    await fs.writeFileSync(
+      path.join(__dirname, '.lyric.json'),
+      JSON.stringify(lyrics),
+      {}
+    );
+  }
   const ed = new Date();
   return {
     pos: playerRes.progress + Number.parseFloat((ed - st) / 1000),
-    lyrics: lyricsRes.lrc.lyric.split('\n'),
+    lyrics: lyrics.lyrics,
   };
 }
 
 function checkLyrics(pos, lyric) {
-  const reg = /^\[([0-9]{2}):([0-9]{2})\.([0-9]*)/.exec(lyric);
-  cur =
-    Number.parseInt(reg[1], 10) * 60 +
-    Number.parseInt(reg[2], 10) +
-    Number.parseInt(reg[3], 10) / 1000;
-  return pos - cur + 0.5;
+  return pos - lyric.time + 0.5;
 }
 
 function findLyrics(pos, lyrics) {
   let l = 0;
-  let r = lyrics.length - 2;
+  let r = lyrics.length - 1;
   while (l < r) {
     const mid = (l + r) >> 1;
     const ck = checkLyrics(pos, lyrics[mid]);
@@ -50,11 +72,10 @@ function findLyrics(pos, lyrics) {
 (async () => {
   const { pos, lyrics } = await reqLyrics();
   const lyricIdx = findLyrics(pos, lyrics);
-  const lyricl =
-    lyrics[lyricIdx].replace(/\[([0-9]{2}):([0-9]{2})\.([0-9]*)\]/, '') ||
-    '...';
-  // const lyricr =
-  //   lyrics[lyricIdx + 1].replace(/\[([0-9]{2}):([0-9]{2})\.([0-9]*)\]/, '') ||
-  //   '...';
+  let lyricl = lyrics[lyricIdx].text;
+  if (!lyricl) lyricl = '...';
   console.log(lyricl);
+  // let lyricr = lyrics[lyricIdx + 1].text;
+  // if (!lyricr) lyricr = '...';
+  // console.log(lyricl + ' / ' + lyricr);
 })();
